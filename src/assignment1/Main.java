51bf27602ba1680f8d0ef37e69138cea4c302058
Park JaeHong <blogerhong@naver.com>
2024-10-11 19:39:48 +0900

    Initialize git

---
+++ src/assignment1/Main.java
@@ -0,0 +1,10 @@
+package assignment1;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+    }
+}
